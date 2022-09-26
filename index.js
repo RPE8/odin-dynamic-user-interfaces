68fe3918339b575819d04f696ff2122d0bbe9cc2
RPE8 <vitali.shuman96@gmail.com>
2022-09-26 19:28:40 +0300

    mobile menu

--- index.js
+++ index.js
@@ -1,1 +1,8 @@
+const menuBackground = document.querySelector('.menu-bg');
+const burgerButton = document.querySelector('.hamb-button');
+const burgerMenu = document.querySelector('.hamb-menu');
 
+burgerButton.addEventListener('click', () => {
+	burgerMenu.classList.toggle('hamb-menu--openned');
+	menuBackground.classList.toggle('menu-bg--openned');
+});
